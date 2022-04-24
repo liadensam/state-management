e44fc6e426f6c2b9c0e3a0b750edf1d40f8fd72e
hebaulf <heba.farestveit@gmail.com>
2022-04-24 23:41:16 +0000

    Add non pages to nex.config. Remove unused import.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['store-zustand.js', 'store-context.js', 'store-context-selector.js',]
 }
 
 module.exports = nextConfig
